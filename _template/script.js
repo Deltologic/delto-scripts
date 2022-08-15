711d955b3bc2e985e65596b87839d67567475333
jakubriegel <jakubriegel112@gmail.com>
2022-08-15 20:48:49 +0200

    DELTO-267: add commnets to templates

--- _template/script.js
+++ _template/script.js
@@ -4,11 +4,15 @@
 
 import { writeFileSync } from 'fs'
 
+// define CSV columns separator
 const separator = ','
 
 function script() {
+    // prepare CSV file structure
     const csvContent = `Have${separator}a${separator}nice${separator}day${separator}!\n`
+    // save the CSV file
     writeFileSync('example.csv', csvContent)
 }
 
+// start the script
 script()
